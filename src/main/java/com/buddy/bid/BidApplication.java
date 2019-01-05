0e259c95458b4ab680a38ad86e47b120ef113480
d817325 <sulabh.rastogi@team.telstra.com>
2019-01-05 14:04:47 +0530

    initial structure

---
+++ src/main/java/com/buddy/bid/BidApplication.java
@@ -0,0 +1,14 @@
+package com.buddy.bid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidApplication.class, args);
+    }
+
+}
+
